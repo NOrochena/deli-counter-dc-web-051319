21c0de77ae5156171aaa440a5c6ab9de06a31956
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-04-30 05:35:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 # Write your code here.
 def line(current_line)
   if (current_line.count == 0)
-    "The line is currently empty."
+    puts "The line is currently empty."
   else
     line = "The line is currently:"
     current_line.each_with_index do |ele, index|
       line += " #{index+1}. #{ele}"
     end
-    line
+    puts line
   end
 end
