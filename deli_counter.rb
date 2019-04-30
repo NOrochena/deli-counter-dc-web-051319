d66249945d3ccc114191a48cd33efc3e091d5f79
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-04-30 05:31:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(current_line)
     "The line is currently empty."
   else
     line = "The line is currently:"
-    current_line.each do |ele|
+    current_line.each_with_index do |ele|
       line += "1. "
     end
   end
